a8acbe52d65214b58c78ac1d0acff4b415208e4c
ignaciochagar <43643985+ignaciochagar@users.noreply.github.com>
2024-04-26 09:24:57 +0200

    Muestro datos en pantalla e iconos de la ciudad

--- script.js
+++ script.js
@@ -14,25 +14,38 @@ let min = document.getElementById('min');
 let max = document.getElementById('max');
 
 //Declarar funciones secundarias
-
 const displayBackgroundImage = (obj) => {
     console.log(obj.dt);
     // extraer y dar formato a la fecha
-    let dateSpanish = new Date(obj.dt*1000).toLocaleString("es-ES",{
-        timeStyle : "short",
-        dateStyle : "long"
+    let dateSpanish = new Date(obj.dt * 1000).toLocaleString("es-ES", {
+        timeStyle: "short",
+        dateStyle: "long"
     });
     console.log(dateSpanish);
-
+    //Manipular el DOM para añadir la hora
+    date.textContent = `Actualizado el: ${dateSpanish}`;//actualizaciondateSpanish;
     //extraer la hora
-    const dayHour = new Date(obj.dt*1000).getHours();
+    const dayHour = new Date(obj.dt * 1000).getHours();
     console.log(dayHour);
+
+    if (dayHour > 6 && dayHour < 19) {
+        container.classList.remove("night");
+        container.classList.add("day");
+    } else {
+        container.classList.remove("day");
+        container.classList.add("night");
+    }
 };
 
-
-
-const displayData = (data) => {
-
+//Capturo los datos del objeto del API y los muestro en el DOM
+const displayData = (obj) => {
+    temperatureDegrees.textContent = Math.floor(obj.main.temp);
+    timeZone.textContent = obj.name;
+    const icon = obj.weather[0].icon;
+    weatherIcon.innerHTML = `<img src='icons/${icon}.png'> </img>`;
+    min.textContent = Math.floor(obj.main.temp_min);
+    max.textContent = Math.floor(obj.main.temp_max);
+    temperatureDescription.textContent = obj.weather[0].description;
 }
 
 //Declarar getWeatherData como una función (principal)
@@ -43,19 +56,16 @@ const getWeatherData = async (city) => {
     //Fetch nos permite hacer peticiones HTTP
     const res = await fetch(`https://open-weather13.p.rapidapi.com/city/${city}/ES`, {
         headers: {
-            'X-RapidAPI-Key': '3d561cfc05msh604560a7aa6e29dp1283e8jsnb38d7646aebe',
+            'X-RapidAPI-Key': 'e5eda50b2bmshc43223fb83f4215p153748jsna802f1eace80',
             'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
         }
-    }   );
+    });
     const data = await res.json();
-
-    console.log(data);
     //Cambiar fondo pantalla de noche o de diá
     displayBackgroundImage(data);
     //Mostrar datos en el DOM
-    // displayData(data);
+    displayData(data);
 }
-
 
 search_form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -63,10 +73,8 @@ search_form.addEventListener('submit', (e) => {
 })
 
 //Al cargar página nos cargue una localización predeterminada
-
 window.onload = () => {
     getWeatherData(search_input.value);
-
 }
 
 
